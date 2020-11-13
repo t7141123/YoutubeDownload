53dc5f6970237c48fcdfca2bde30331970a72dea
t7141123 <t71411@gmail.com>
2020-11-14 00:27:44 +0800

    Add PlayList function

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,6 +1,10 @@
 from pytube import YouTube
+#from pytube import Playlist
 import sys
 
+plURL = input("Please type your play list: ")
 yt = YouTube(sys.argv[1])
 yt.streams.first().download()
+#pl = Playlist(plURL)
+#pl.download_all()
 
